a2f73a25569194ccc70ae586d1b59af4c9a3c032
vtinter <156923123+NguyenvantuanT@users.noreply.github.com>
2024-06-08 20:38:37 +0700

    23

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -5,4 +5,5 @@ const  adminController = require('../app/controllers/AdminController')
 router.get('/admin', adminController.ShowIndexAdmin);
 router.get('/admin/addsanpham', adminController.addsanpham);
 router.post('/admin/added', adminController.added);
+router.get('/admin/update', adminController.updatesanpham);
 module.exports = router;
